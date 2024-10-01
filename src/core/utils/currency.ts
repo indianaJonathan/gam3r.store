5b981c69b0ba2c5b514d6a815d25a097bfe87833
Jonathan Hermam <jherman@outlook.com.br>
2024-10-01 03:06:29 -0300

    fix: Fixed a linting problem at currency util

--- src/core/utils/currency.ts
+++ src/core/utils/currency.ts
@@ -2,4 +2,8 @@ export default class Currency {
     static format(value: number, locale = "pt-BR", currency = "BRL"): string {
         return (value ?? 0).toLocaleString(locale, { style: "currency", currency })
     }
+
+    toString(value: number): string {
+        return String(value);
+    }
 }
